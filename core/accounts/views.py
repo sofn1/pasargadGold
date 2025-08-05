95f6143e45c62c9b2d38d9577b69dfc06327d22d
mehrdad <m9hrdad.art@gmail.com>
2025-08-05 18:17:19 +0330

    primary commit - frontend

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -47,9 +47,9 @@ def login_user(request, role, template_name):
 
         # Redirects based on role
         if role == 'admin' and user.is_superuser:
-            return redirect('/admin-dashboard/superadmin-dashboard/')
+            return redirect('/superadmin-dashboard/')
         elif role == 'admin':
-            return redirect('/admin-dashboard/admin-dashboard/')
+            return redirect('/admin-dashboard/')
         elif role == 'writer':
             return redirect('/writer-dashboard/')
         elif role == 'seller':
