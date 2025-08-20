87606089b04aaf890f79184f555c9503fe0df26b
mehrdad <sernacompanytech@gmail.com>
2025-08-20 15:18:02 +0330

    serna modifications

--- core/core/settings.py
+++ core/core/settings.py
@@ -93,6 +93,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
                 "core.context_processors.site_colors",
             ],
         },
@@ -161,6 +162,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # ✅ This is the missing part
+
+# MEDIA (user-uploaded files)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
